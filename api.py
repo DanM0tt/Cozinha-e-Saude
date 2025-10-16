9f035e2fbcd9c6c439e6c9061841c27db0389c4f
MClarapg <mcpg@cin.ufpe.br>
2025-10-16 13:42:38 -0300

    Nova integração da API + instruções atualizadas

--- api.py
+++ api.py
@@ -1,35 +1,29 @@
-from google import genai
-from pydantic import BaseModel
-from dotenv import load_dotenv
-import os
+# Bibliotecas importadas
+import google.generativeai as gemini_ai
+import os # Biblioteca para acessar o arquivo .env
+from dotenv import load_dotenv # Importe load_dotenv
 
-load_dotenv()
-api_key = os.getenv("API_KEY")
+load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env
 
-class Receita(BaseModel):
-    nome_receita: str
-    ingredientes: list[str]
+# A chamada da API
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+gemini_ai.configure(api_key=GEMINI_API_KEY)
 
-prompt = input("Quais ingredientes você possui para essa receita?")
+# A parte abaixo apenas printa os modelos (fica caso queiram mudar o modelo depois)
+#    modelos = gemini_ai.list_models()
+#    for model in modelos:
+#        if "generateContent" in model.supported_generation_methods:
+#            print(f"Nome do Modelo: {model.name}")
+#
+model = gemini_ai.GenerativeModel('gemini-2.5-flash') # o modelo escolhido / pode ser alterado
 
-client = genai.Client(api_key = f"{api_key}")
-resposta = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=f"Quero que você crie uma receita com os ingredientes que tenho disponível, sendo eles os seguintes {prompt}",
-    config={
-        "response_mime_type": "application/json",
-        "response_schema": list[Receita],
-    },
-)
+# A estrutura de geração de resposta é a seguinte: resposta = model.generate_content( <prompt> )
+ingredientes_disponiveis = input("Digite os alimentos disponíveis: ")
+qnt_porcoes = int(input("Informe a quantidade de porções desejada: "))
+restricao = input("Informe quaisquer restrições alimentares que você tenha (ex: vegetariano, intolerante a lactose): ")
 
-minhas_receitas: list[Receita] = resposta.parsed
+prompt = f"Gere uma receita apenas com os ingredientes: {ingredientes_disponiveis}, fornecendo informações como quantidade, tempo de preparo e utensílios necessários para uma receita que sirva {qnt_porcoes} porções. Evite as seguintes restrições: {restricao}"
 
-print()
-
-for receita in minhas_receitas:
-    print(f"{receita.nome_receita}:")
-
-    for ingred in receita.ingredientes:
-        print(f"\t- {ingred}")
-
-    print()
+# Geração da receita
+resposta = model.generate_content(prompt)
+print(resposta.text) # Resposta em texto
